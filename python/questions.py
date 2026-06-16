2fa16b2b9ac963b888b91e992ac27469956f7cf1
Mahicode0712 <cloud.mahisingh@gmail.com>
2026-06-16 10:35:41 +0530

    feat:exception handling

--- python/questions.py
+++ python/questions.py
@@ -1,11 +1,22 @@
+# try:
+#     num1=float(input("enter the number: "))
+#     num2=float(input("enter the number: "))
+
+
+#     result = num1/num2
+#     print("the result is " , result)
+
+# except ZeroDivisionError:
+#     print("Error : the division from zero is not defined ")
+# print(" try again ")        
+
+#invalid integer
 try:
-    num1=float(input("enter the number: "))
-    num2=float(input("enter the number: "))
+
+   num = int(input("enter the number: "))
+   print("you entered the number : " , num)
+except ValueError:
+        print("enter a valid number")
 
 
-    result = num1/num2
-    print("the result is " , result)
 
-except ZeroDivisionError:
-    print("Error : the division from zero is not defined ")
-print(" try again ")        
